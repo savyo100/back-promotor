09cb3162f42672223072c5be05ddd64d3a3cbfbe
savyo100 <savyo.br09@gmail.com>
2026-01-30 11:43:40 -0300

    Fix typo in server.ts variable declaration

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/", routes);
 app.use('/auth', authRoutes);
 
-cconst PORT = Number(process.env.PORT) || 3333;
+const PORT = Number(process.env.PORT) || 3333;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
