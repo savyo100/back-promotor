7fa94e9d8b6fd6e4ea99b9fc8e2eeb0e656336af
savyo100 <savyo.br09@gmail.com>
2026-01-30 11:41:48 -0300

    Fix server port variable declaration and logging

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ app.use(express.json());
 app.use("/", routes);
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 3333;
-app.listen(PORT,'0.0.0.0', () =>
-  console.log(`Server running on port {PORT}`)
-);
+cconst PORT = Number(process.env.PORT) || 3333;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
